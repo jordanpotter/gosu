8424bc737c0d26a93d73092322f6f0385d60abf7
Jordan Potter <me@jordanpotter.com>
2015-02-21 19:01:08 -0800

    Add Godep for dependency management. Add Gin web framework dependency

--- api/api.go
+++ api/api.go
@@ -1,7 +1,14 @@
 package main
 
 import "fmt"
+import "github.com/gin-gonic/gin"
 
 func main() {
 	fmt.Println("api")
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
+	})
+	r.Run(":8080")
 }
