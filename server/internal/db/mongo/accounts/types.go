bed3afc92ffda2543a230653024a8f446c913849
Jordan Potter <me@jordanpotter.com>
2015-05-06 22:28:34 -0700

    Add membership definition to db types

--- server/internal/db/mongo/accounts/types.go
+++ server/internal/db/mongo/accounts/types.go
@@ -9,9 +9,10 @@ import (
 )
 
 type storedAccount struct {
-	Id      bson.ObjectId  `bson:"_id,omitempty"`
-	Email   string         `bson:"email"`
-	Devices []storedDevice `bson:"devices"`
+	Id          bson.ObjectId      `bson:"_id,omitempty"`
+	Email       string             `bson:"email"`
+	Devices     []storedDevice     `bson:"devices"`
+	Memberships []storedMembership `bson:"memberships"`
 }
 
 type storedDevice struct {
@@ -20,16 +21,30 @@ type storedDevice struct {
 	Created      time.Time `bson:"created"`
 }
 
+type storedMembership struct {
+	RoomId   bson.ObjectId `bson:"_id,omitempty"`
+	PeerName string        `bson:"peerName"`
+	Admin    bool          `bson:"admin"`
+	Banned   bool          `bson:"banned"`
+	Created  time.Time     `bson:"created"`
+}
+
 func (sa *storedAccount) toAccount() *db.Account {
 	devices := make([]db.Device, len(sa.Devices))
 	for _, sDevice := range sa.Devices {
 		devices = append(devices, *sDevice.toDevice())
 	}
 
+	memberships := make([]db.Membership, len(sa.Memberships))
+	for _, sMembership := range sa.Memberships {
+		memberships = append(memberships, *sMembership.toMembership())
+	}
+
 	return &db.Account{
-		Id:      sa.Id.Hex(),
-		Email:   sa.Email,
-		Devices: devices,
+		Id:          sa.Id.Hex(),
+		Email:       sa.Email,
+		Devices:     devices,
+		Memberships: memberships,
 	}
 }
 
@@ -38,5 +53,15 @@ func (sd *storedDevice) toDevice() *db.Device {
 		Name:         sd.Name,
 		PasswordHash: sd.PasswordHash,
 		Created:      sd.Created,
+	}
+}
+
+func (sm *storedMembership) toMembership() *db.Membership {
+	return &db.Membership{
+		RoomId:   sm.RoomId.Hex(),
+		PeerName: sm.PeerName,
+		Admin:    sm.Admin,
+		Banned:   sm.Banned,
+		Created:  sm.Created,
 	}
 }
