d0d4573b236010517f1bac6bd5255e1f4920c31d
Jordan Potter <me@jordanpotter.com>
2015-05-09 20:32:02 -0700

    Remove any previous membership before creating new one

--- server/internal/db/mongo/accounts/update.go
+++ server/internal/db/mongo/accounts/update.go
@@ -7,10 +7,15 @@ import (
 )
 
 func (c *conn) AddMembership(id, roomId, peerName string) error {
+	err := c.RemoveMembership(id, roomId)
+	if err != nil {
+		return err
+	}
+
 	membershipBson := bson.M{"roomId": bson.ObjectIdHex(roomId), "peerName": peerName, "created": time.Now()}
 	dataBson := bson.M{"$push": bson.M{"memberships": membershipBson}}
 	col := c.session.DB(c.config.Name).C(c.config.Collections.Accounts)
-	err := col.UpdateId(bson.ObjectIdHex(id), dataBson)
+	err = col.UpdateId(bson.ObjectIdHex(id), dataBson)
 	return err
 }
 
