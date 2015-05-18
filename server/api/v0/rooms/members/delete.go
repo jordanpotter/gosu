ee7e8cfc01ed66b699342173e8947b2380939620
Jordan Potter <me@jordanpotter.com>
2015-05-17 21:28:47 -0700

    Add commands to modify room members

--- server/api/v0/rooms/members/delete.go
+++ server/api/v0/rooms/members/delete.go
@@ -4,12 +4,23 @@ import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
-	"github.com/jordanpotter/gosu/server/internal/db"
+	"github.com/jordanpotter/gosu/server/api/middleware"
 )
 
 func (h *Handler) leave(c *gin.Context) {
 	roomID := c.Params.ByName("roomID")
-	fmt.Printf("TODO: leave room %s", roomID)
+	accountID, err := c.Get(middleware.AccountIDKey)
+	if err != nil {
+		c.Fail(500, err)
+		return
+	}
+
+	err = h.dbConn.Rooms.RemoveMemberByAccount(roomID, accountID.(string))
+	if err != nil {
+		c.Fail(500, err)
+		return
+	}
+
 	c.String(200, "ok")
 }
 
@@ -20,10 +31,7 @@ func (h *Handler) delete(c *gin.Context) {
 	roomID := c.Params.ByName("roomID")
 	memberID := c.Params.ByName("memberID")
 	err := h.dbConn.Rooms.RemoveMember(roomID, memberID)
-	if err == db.NotFoundError {
-		c.Fail(404, err)
-		return
-	} else if err != nil {
+	if err != nil {
 		c.Fail(500, err)
 		return
 	}
