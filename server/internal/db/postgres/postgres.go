211db4d2c8eb6a7401872f5279237a99a9edb561
Jordan Potter <me@jordanpotter.com>
2015-06-07 19:40:50 -0700

    Begin supporting running migrations from code

--- server/internal/db/postgres/postgres.go
+++ server/internal/db/postgres/postgres.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
+	"github.com/mattes/migrate/migrate"
 
 	"github.com/jordanpotter/gosu/server/internal/config"
 	"github.com/jordanpotter/gosu/server/internal/db"
@@ -12,13 +13,18 @@ import (
 
 func New(addrs []config.PostgresNode, config *config.Postgres) (*db.Conn, error) {
 	fmt.Println("TODO: support multiple postgres endpoints")
-	postgresConn, err := getConnWithAddr(addrs[0], config)
+	postgres, err := getDBWithAddr(addrs[0], config)
+	if err != nil {
+		return nil, err
+	}
+
+	err = performMigrations(addrs[0], config, "./conf/db/migrations")
 	if err != nil {
 		return nil, err
 	}
 
 	conn := &db.Conn{
-		Closer: postgresConn,
+		Closer: postgres,
 	}
 	return conn, nil
 
@@ -40,8 +46,18 @@ func New(addrs []config.PostgresNode, config *config.Postgres) (*db.Conn, error)
 	// return conn, nil
 }
 
-func getConnWithAddr(addr config.PostgresNode, config *config.Postgres) (*sqlx.DB, error) {
-	connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",
-		addr.IP.String(), addr.DBPort, config.Username, config.Password, config.Name, config.SSLMode)
+func getDBWithAddr(addr config.PostgresNode, config *config.Postgres) (*sqlx.DB, error) {
+	connStr := fmt.Sprintf("host=%s port=%d dbname=%s user=%s password=%s sslmode=%s",
+		addr.IP.String(), addr.DBPort, config.Name, config.Username, config.Password, config.SSLMode)
 	return sqlx.Connect("postgres", connStr)
+}
+
+func performMigrations(addr config.PostgresNode, config *config.Postgres, migrationsPath string) error {
+	connStr := fmt.Sprintf("postgres://%s:%d/%s?user=%s&password=%s&sslmode=%s",
+		addr.IP.String(), addr.DBPort, config.Name, config.Username, config.Password, config.SSLMode)
+	allErrors, ok := migrate.UpSync(connStr, migrationsPath)
+	if !ok {
+		return allErrors[0]
+	}
+	return nil
 }
