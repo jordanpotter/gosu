2f9994aeec6ff96d722671aad1360595e4c297b3
Jordan Potter <me@jordanpotter.com>
2015-06-07 22:46:34 -0700

    Create first migration files

---
+++ conf/db/migrations/001_initial_schema.up.sql
@@ -0,0 +1,58 @@
+CREATE TABLE accounts (
+    id              integer PRIMARY KEY,
+    email           varchar(256) UNIQUE NOT NULL,
+    created         timestamp without time zone NOT NULL
+);
+
+CREATE INDEX idx_account_email ON accounts ((lower(email)));
+
+
+CREATE TABLE devices (
+    id              integer PRIMARY KEY,
+    account_id      integer REFERENCES accounts(id) NOT NULL,
+    name            varchar(100) NOT NULL,
+    password_hash   bytea NOT NULL,
+    created         timestamp without time zone NOT NULL,
+    last_login      timestamp without time zone
+);
+
+CREATE INDEX idx_device_account_id ON devices (account_id);
+
+
+CREATE TABLE rooms (
+    id              integer PRIMARY KEY,
+    name            varchar(25) UNIQUE NOT NULL,
+    password_hash   bytea NOT NULL,
+    created         timestamp without time zone NOT NULL
+);
+
+CREATE INDEX idx_room_name ON rooms (name);
+
+
+CREATE TABLE channels (
+    id              integer PRIMARY KEY,
+    room_id         integer REFERENCES rooms(id) NOT NULL,
+    name            varchar(25) NOT NULL,
+    created         timestamp without time zone NOT NULL,
+    UNIQUE (room_id, name)
+);
+
+CREATE INDEX idx_channel_room_id ON channels (room_id);
+
+
+CREATE TABLE members (
+    id              integer PRIMARY KEY,
+    room_id         integer REFERENCES rooms(id) NOT NULL,
+    account_id      integer REFERENCES accounts(id) NOT NULL,
+    channel_id      integer REFERENCES channels(id),
+    name            varchar(25) NOT NULL,
+    admin           boolean NOT NULL,
+    banned          boolean NOT NULL,
+    created         timestamp without time zone NOT NULL,
+    last_login      timestamp without time zone,
+    UNIQUE (room_id, account_id),
+    UNIQUE (room_id, name)
+);
+
+CREATE INDEX idx_member_room_id ON members (room_id);
+CREATE INDEX idx_member_account_id ON members (account_id);
