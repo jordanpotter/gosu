a926622609b32a37220f6bb71363a0a9d4e8f873
Jordan Potter <me@jordanpotter.com>
2015-06-14 14:13:44 -0700

    Add comment to schema that password_hash uses bcrypt

--- conf/db/migrations/001_initial_schema.up.sql
+++ conf/db/migrations/001_initial_schema.up.sql
@@ -11,7 +11,7 @@ CREATE TABLE devices (
     id              serial PRIMARY KEY,
     account_id      integer REFERENCES accounts(id) NOT NULL,
     name            varchar(100) NOT NULL,
-    password_hash   bytea NOT NULL,
+    password_hash   bytea NOT NULL, -- use bcrypt
     created         timestamp without time zone NOT NULL,
     last_login      timestamp without time zone
 );
@@ -22,7 +22,7 @@ CREATE INDEX idx_device_account_id ON devices (account_id);
 CREATE TABLE rooms (
     id              serial PRIMARY KEY,
     name            varchar(25) UNIQUE NOT NULL,
-    password_hash   bytea NOT NULL,
+    password_hash   bytea NOT NULL, -- use bcrypt
     created         timestamp without time zone NOT NULL
 );
 
@@ -42,8 +42,8 @@ CREATE INDEX idx_channel_room_id ON channels (room_id);
 
 CREATE TABLE members (
     id              serial PRIMARY KEY,
-    room_id         integer REFERENCES rooms(id) NOT NULL,
     account_id      integer REFERENCES accounts(id) NOT NULL,
+    room_id         integer REFERENCES rooms(id) NOT NULL,
     channel_id      integer REFERENCES channels(id),
     name            varchar(25) NOT NULL,
     admin           boolean NOT NULL,
